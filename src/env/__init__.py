600f0f8f7bf14ddd7e8d1ad5390a2ad4cd82225f
Mohsen Bellaj <bellajmohsen07@gmail.com>
2026-09-20 00:31:26 +0100

    M3 - Task Generator with 4 Workload Profiles (#11)

--- src/env/__init__.py
+++ src/env/__init__.py
@@ -3,6 +3,13 @@ from src.env.link import Link, skyfield_is_visible
 from src.env.radiation import RadiationModel, FailureMode
 from src.env.psplib_loader import load_psplib, load_all_psplib
 from src.env.topology import build_topology, build_nodes, build_links, Topology
+from src.env.task_generator import (
+    generate_burst,
+    generate_energy,
+    generate_radiation,
+    generate_mixed,
+    generate_all_profiles,
+)
 
 __all__ = [
     "Node",
@@ -16,4 +23,9 @@ __all__ = [
     "build_nodes",
     "build_links",
     "Topology",
+    "generate_burst",
+    "generate_energy",
+    "generate_radiation",
+    "generate_mixed",
+    "generate_all_profiles",
 ]
